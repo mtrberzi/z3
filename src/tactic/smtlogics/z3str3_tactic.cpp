5cf2ab2e4bcc9342e9c4d429eedeb2ac101b685c
Murphy Berzish <murphy.berzish@gmail.com>
2019-01-18 17:38:49 -0500

    z3str3 tactic: use z3str3 for 15 seconds, then try theory_seq

--- src/tactic/smtlogics/z3str3_tactic.cpp
+++ src/tactic/smtlogics/z3str3_tactic.cpp
@@ -20,9 +20,15 @@ Notes:
 #include "tactic/tactical.h"
 #include "tactic/core/simplify_tactic.h"
 #include "smt/tactic/smt_tactic.h"
+#include "smt/params/smt_params.h"
 
 tactic * mk_z3str3_tactic(ast_manager & m, params_ref const & p) {
     TRACE("str", tout << "using Z3str3 portfolio tactic" << std::endl;);
+    smt_params m_smt_params;
+    m_smt_params.updt_params(p);
+
+    TRACE("str", tout << "timeout = " << m_smt_params.m_timeout << std::endl;);
+
     tactic * preamble = mk_simplify_tactic(m, p);
 
     params_ref preprocess_p = p;
@@ -35,8 +41,13 @@ tactic * mk_z3str3_tactic(ast_manager & m, params_ref const & p) {
     general_p.set_bool("str.fixed_length_models", false);
     general_p.set_sym("string_solver", symbol("z3str3"));
 
+    params_ref seq_p = p;
+    seq_p.set_sym("string_solver", symbol("seq"));
+
     tactic * st = using_params(and_then(preamble, or_else(
-            and_then(using_params(mk_smt_tactic(m), preprocess_p), mk_fail_if_undecided_tactic()),
-            using_params(mk_smt_tactic(m), general_p))), p);
+            using_params(mk_smt_tactic(m), preprocess_p),
+            try_for(using_params(mk_smt_tactic(m), general_p), 15000),
+            using_params(mk_smt_tactic(m), seq_p)
+            )), p);
     return st;
 }
