693a48811ff519c513f1a8e68b176a06bb509ce2
Federico Mora <fmorarocha@gmail.com>
2019-07-03 15:08:06 -0400

    ext_str: find substitutions and then rewrite

--- src/tactic/str/ext_str_tactic.cpp
+++ src/tactic/str/ext_str_tactic.cpp
@@ -19,169 +19,140 @@ Notes:
 #include "tactic/str/ext_str_tactic.h"
 #include "tactic/generic_model_converter.h"
 #include "ast/ast_pp.h"
-
-// return the new expression, keep track of fresh variables used to ignore them in the model, add new assertions to side.
-static expr * ext_str_helper(ast_manager &m, expr_ref_vector & fresh, expr * curr, expr_ref_vector & side)
-{
-    seq_util u(m);
-    arith_util m_autil(m);
-
-    TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-
-    if (m.is_not(curr))
-    {
-        expr * child;
-        m.is_not(curr, child);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m.mk_not(ext_str_helper(m , fresh, child, side));
-    }
-    else if (m.is_and(curr)) 
-    {
-        expr * lhs, * rhs;
-        m.is_and(curr, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m.mk_and(ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }
-    else if (m.is_or(curr)) 
-    {
-        expr * lhs, * rhs;
-        m.is_or(curr, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m.mk_or(ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }
-    else if (m.is_ite(curr)) 
-    {
-        expr * cond, * lhs, * rhs;
-        m.is_ite(curr, cond, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m.mk_ite(ext_str_helper(m , fresh, cond, side), ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }
-    else if (m.is_implies(curr)) 
-    {
-        expr * lhs, * rhs;
-        m.is_implies(curr, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m.mk_implies(ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }    
-    else if (m.is_iff(curr)) 
-    {
-        expr * lhs, * rhs;
-        m.is_iff(curr, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m.mk_iff(ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }
-    else if (m.is_eq(curr))
-    {
-        expr * lhs, * rhs;
-        m.is_eq(curr, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m.mk_eq(ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }
-    else if (u.str.is_concat(curr))
-    {
-        expr * lhs, * rhs;
-        u.str.is_concat(curr, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return u.str.mk_concat(ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }
-    else if (u.str.is_prefix(curr))
-    {
-        expr * needle, * haystack;
-        u.str.is_prefix(curr, needle, haystack);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return u.str.mk_prefix(ext_str_helper(m , fresh, needle, side), ext_str_helper(m , fresh, haystack, side));
-    }
-    else if (u.str.is_suffix(curr))
-    {
-        expr * needle, * haystack;
-        u.str.is_suffix(curr, needle, haystack);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return u.str.mk_suffix(ext_str_helper(m , fresh, needle, side), ext_str_helper(m , fresh, haystack, side));
-    }
-    else if (u.str.is_contains(curr))
-    {
-        expr * needle, * haystack;
-        u.str.is_contains(curr, haystack, needle);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return u.str.mk_contains(ext_str_helper(m , fresh, haystack, side), ext_str_helper(m , fresh, needle, side));
-    }
-    else if (u.str.is_extract(curr))
-    {
-        expr * y, *n, *l;
-        u.str.is_extract(curr, y, n, l);
-        // make a fresh string variable and put it in place of the ext_string
-        app * v;
-        v = m.mk_fresh_const(nullptr, u.str.mk_string_sort());
-        fresh.push_back(v);
-        expr_ref x(v, m);
-
-        // inject auxiliary lemma
-        v = m.mk_fresh_const(nullptr, u.str.mk_string_sort());
-        fresh.push_back(v);
-        expr_ref z1(v, m);
-        v = m.mk_fresh_const(nullptr, u.str.mk_string_sort());
-        fresh.push_back(v);
-        expr_ref z2(v, m);
-
-        expr_ref zero(m_autil.mk_numeral(rational(0), true), m);
-        symbol sym("");
-        expr_ref empty_str(u.str.mk_string(sym), m);
-        expr_ref len_y(u.str.mk_length(y), m);
-        expr_ref len_z1(u.str.mk_length(z1), m);
-
-        expr_ref cond(m.mk_and(m_autil.mk_ge(n, zero), m_autil.mk_ge(n, zero), m_autil.mk_gt(m_autil.mk_add(n, len_y), zero)), m);
-        expr_ref then(m.mk_and(m.mk_eq(y, u.str.mk_concat(z1, x, z2)), m.mk_eq(len_z1, n)), m);
-        expr_ref el(m.mk_eq(x, empty_str), m);
-
-        TRACE("ext_str_debug", tout << "if " << mk_pp(cond, m) << std::endl;);
-        TRACE("ext_str_debug", tout << "then " << mk_pp(then, m) << std::endl;);
-        TRACE("ext_str_debug", tout << "else " << mk_pp(el, m) << std::endl;);
-
-        side.push_back(m.mk_ite(cond, then, el));
-
-        TRACE("ext_str_debug", tout << "curr replaced by: " << mk_pp(x, m) << std::endl;);
-        return x;
-    } 
-    else if (u.str.is_length(curr))
-    {
-        expr * child;
-        u.str.is_length(curr, child);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return u.str.mk_length(ext_str_helper(m , fresh, child, side));
-    }
-    else if (m_autil.is_add(curr))
-    {
-        expr * lhs, * rhs;
-        m_autil.is_add(curr, lhs, rhs);
-        TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-        return m_autil.mk_add(ext_str_helper(m , fresh, lhs, side), ext_str_helper(m , fresh, rhs, side));
-    }
-    
-    TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
-    return curr;
-}
-
+#include "ast/rewriter/expr_replacer.h"
 
 class ext_str_tactic : public tactic {
+
     struct imp {
 
         typedef generic_model_converter mc;
 
-        ast_manager &     m;
-        seq_util          u;
-        expr_ref_vector   m_fresh_vars;
-        ref<mc>           m_mc;
-        bool              m_produce_models;
+        ast_manager &              m;
+        seq_util                   u;
+        arith_util                 m_autil;
+        expr_ref_vector            m_fresh_vars;
+        ptr_vector<expr>           stack;
+        scoped_ptr<expr_replacer>  m_replace;
+        ref<mc>                    m_mc;
+        bool                       m_produce_models;
 
         imp(ast_manager & _m, params_ref const & p):
             m(_m),
             u(m),
+            m_autil(m),
             m_fresh_vars(m) {
+            m_replace = mk_default_expr_replacer(m);
             updt_params(p);
         }
 
         void updt_params(params_ref const & p) {
 
+        }
+
+        void process_logical_connective(expr * op, goal_ref const & g, expr_substitution & sub)
+        {
+            
+        }
+
+        void process_eq(expr * eq, goal_ref const & g, expr_substitution & sub)
+        {
+            expr * lhs, * rhs;
+            m.is_eq(eq, lhs, rhs);
+            stack.push_back(lhs);
+            stack.push_back(rhs);
+        }
+
+        void process_concat(expr * concat, goal_ref const & g, expr_substitution & sub)
+        {
+            expr * lhs, * rhs;
+            u.str.is_concat(concat, lhs, rhs);
+            stack.push_back(lhs);
+            stack.push_back(rhs);
+        }
+
+        void process_prefix(expr * prefix, goal_ref const & g, expr_substitution & sub)
+        {
+            expr * lhs, * rhs;
+            u.str.is_prefix(prefix, lhs, rhs);
+            stack.push_back(lhs);
+            stack.push_back(rhs);
+        }
+
+        void process_suffix(expr * suffix, goal_ref const & g, expr_substitution & sub)
+        {
+            expr * lhs, * rhs;
+            u.str.is_suffix(suffix, lhs, rhs);
+            stack.push_back(lhs);
+            stack.push_back(rhs);
+        }
+
+        void process_contains(expr * contains, goal_ref const & g, expr_substitution & sub)
+        {
+            expr * lhs, * rhs;
+            u.str.is_contains(contains, lhs, rhs);
+            stack.push_back(lhs);
+            stack.push_back(rhs);
+        }
+
+        void process_extract(expr * extract, goal_ref const & g, expr_substitution & sub)
+        {
+            expr * y, *n, *l;
+            u.str.is_extract(extract, y, n, l);
+            // make a fresh string variable and put it in place of the ext_string
+            app * v;
+            v = m.mk_fresh_const(nullptr, u.str.mk_string_sort());
+            m_fresh_vars.push_back(v);
+            expr_ref x(v, m);
+
+            // inject auxiliary lemma
+            v = m.mk_fresh_const(nullptr, u.str.mk_string_sort());
+            m_fresh_vars.push_back(v);
+            expr_ref z1(v, m);
+            v = m.mk_fresh_const(nullptr, u.str.mk_string_sort());
+            m_fresh_vars.push_back(v);
+            expr_ref z2(v, m);
+
+            expr_ref zero(m_autil.mk_numeral(rational(0), true), m);
+            symbol sym("");
+            expr_ref empty_str(u.str.mk_string(sym), m);
+            expr_ref len_y(u.str.mk_length(y), m);
+            expr_ref len_z1(u.str.mk_length(z1), m);
+
+            expr_ref c(m.mk_and(m_autil.mk_ge(n, zero), m_autil.mk_ge(n, zero), m_autil.mk_gt(m_autil.mk_add(n, len_y), zero)), m);
+            expr_ref then(m.mk_and(m.mk_eq(y, u.str.mk_concat(z1, x, z2)), m.mk_eq(len_z1, n)), m);
+            expr_ref el(m.mk_eq(x, empty_str), m);
+
+            TRACE("ext_str_debug", tout 
+                << "if\n\t" << mk_pp(c, m) 
+                << "\nthen\n\t" << mk_pp(then, m) 
+                << "\nelse\n\t" << mk_pp(el, m) << std::endl;);
+
+            g->assert_expr(m.mk_ite(c, then, el));
+
+            // TRACE("ext_str_debug", tout << "curr replaced by: " << mk_pp(x, m) << std::endl;);
+            // return x;
+            stack.push_back(y);
+            stack.push_back(n);
+            stack.push_back(l);
+
+            sub.insert(extract, x);
+        }
+
+        void process_length(expr * length, goal_ref const & g, expr_substitution & sub)
+        {
+            expr * child;
+            u.str.is_length(length, child);
+            stack.push_back(child);
+        }
+
+        void process_arith_op(expr * op, goal_ref const & g, expr_substitution & sub)
+        {
+            
+        }
+
+        void process_arith_comparison(expr * comparison, goal_ref const & g, expr_substitution & sub)
+        {
+            
         }
 
         void operator()(goal_ref const & g, goal_ref_buffer & result) {
@@ -206,27 +177,78 @@ class ext_str_tactic : public tactic {
                 return;
             }
 
-            expr_ref_vector side(m);
+            expr_substitution sub(m);
+
             unsigned idx = 0;
             while (idx < g->size()) {
                 if (g->inconsistent())
                     break;
-                expr_ref curr(g->form(idx), m);
-                TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
+                expr * curr = g->form(idx);
 
                 if (is_app(curr)) {
-                    side.reset();
-                    expr_ref new_curr(ext_str_helper(m, m_fresh_vars, curr, side), m);
-                    TRACE("ext_str_debug", tout << "curr replaced by: " << mk_pp(new_curr, m) << std::endl;);
-                    g->update(idx, new_curr);
-                    for (expr * e : side)
+                    stack.reset();
+                    stack.push_back(curr);
+                    while (!stack.empty())
                     {
-                        TRACE("ext_str_debug", tout << "asserting: " << mk_pp(e, m) << std::endl;);
-                        g->assert_expr(e);
+                        curr = stack.back();
+                        stack.pop_back();
+                        if (m.is_not(curr) || m.is_and(curr) || m.is_or(curr) || m.is_ite(curr) || m.is_iff(curr) || m.is_implies(curr))
+                        {
+                            process_logical_connective(curr, g, sub);
+                        }
+                        else if (m.is_eq(curr))
+                        {
+                            //possible not string equality
+                            process_eq(curr, g, sub);
+                        }
+                        else if (u.str.is_concat(curr))
+                        {
+                            process_concat(curr, g, sub);
+                        }
+                        else if (u.str.is_prefix(curr))
+                        {
+                            process_prefix(curr, g, sub);
+                        }
+                        else if (u.str.is_suffix(curr))
+                        {
+                            process_suffix(curr, g, sub);
+                        }
+                        else if (u.str.is_contains(curr))
+                        {
+                            process_contains(curr, g, sub);
+                        }
+                        else if (u.str.is_extract(curr))
+                        {
+                            process_extract(curr, g, sub);
+                        } 
+                        else if (u.str.is_length(curr))
+                        {
+                            process_length(curr, g, sub);
+                        }
+                        else if (m_autil.is_add(curr) || m_autil.is_sub(curr) || m_autil.is_mul(curr))
+                        {
+                            process_arith_op(curr, g, sub);
+                        }
+                        else if (m_autil.is_ge(curr) || m_autil.is_gt(curr) || m_autil.is_le(curr) || m_autil.is_lt(curr))
+                        {
+                            process_arith_comparison(curr, g, sub);
+                        }
                     }
                 }
                 idx++;
-                TRACE("ext_str_debug", tout << "curr " << mk_pp(curr, m) << std::endl;);
+            }
+
+            m_replace->set_substitution(&sub);
+
+            for (unsigned i = 0; i < g->size(); i++) {            
+                expr_ref   new_curr(m);
+                proof_ref  new_pr(m);  
+                (* m_replace)(g->form(i), new_curr, new_pr);
+                if (m.proofs_enabled() && !new_pr) {
+                    new_pr = m.mk_rewrite(g->form(i), new_curr);
+                    new_pr = m.mk_modus_ponens(g->pr(i), new_pr);
+                }
+                g->update(i, new_curr, new_pr, g->dep(i));
             }
 
             if (m_mc) 
@@ -236,14 +258,15 @@ class ext_str_tactic : public tactic {
                 }
             }
 
-            if (m_produce_models && !m_fresh_vars.empty()) 
+            if (m_produce_models && !m_fresh_vars.empty())
+            {
                 g->add(m_mc.get());
+            }
             g->inc_depth();
             result.push_back(g.get());
             SASSERT(g->is_well_sorted());
-            SASSERT(g->is_well_sorted());
             TRACE("ext_str", tout << "AFTER: " << std::endl; g->display(tout);
-                  if (g->mc()) g->mc()->display(tout); tout << std::endl; );
+            if (g->mc()) g->mc()->display(tout); tout << std::endl; );
         }
     };
 
